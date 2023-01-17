af8a8703d92680604e112c6f567c0bffeffaf6d9
Mircea Mihai Adrian <62558071+ayymomma@users.noreply.github.com>
2023-01-17 14:29:56 +0200

    add fetch to get total area

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -3,8 +3,29 @@ import Sidenav from './Sidenav';
 import "./DashBoard.css";
 import { Grid, Box } from '@mui/material';
 import MyCard from '../lib/MyCard';
+import { useEffect } from 'react';
+const url = 'http://localhost:8000/api';
 
 export default function DashBoard() {
+    const [totalArea, setTotalArea] = React.useState(0);
+
+    const fetchDashboardData = () => {
+        fetch(url + "/person/get_all_area", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: 'Bearer ' + localStorage.getItem('token'),
+            }
+        }).then((res) => res.json())
+        .then((data) => { 
+            setTotalArea(data.area);
+        })
+    }
+
+    useEffect(() => {
+        fetchDashboardData();
+    }, []);
     
     return (
         <div className='dashboardPage'>
@@ -16,7 +37,7 @@ export default function DashBoard() {
             <main className='dashboard'>
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container spacing={2} columns={12}>
-                        <MyCard value="100ha" title="Total area" description="All time area" image="https://static-00.iconduck.com/assets.00/results-icon-256x256-cb2c2kkw.png"/>
+                        <MyCard value={totalArea} title="Total area" description="All time area" image="https://static-00.iconduck.com/assets.00/results-icon-256x256-cb2c2kkw.png"/>
                         <MyCard value="10" title="Lease given" description="This year" image="https://static-00.iconduck.com/assets.00/results-icon-256x256-cb2c2kkw.png"/>
                         <MyCard value="10" title="New tenants" description="This year" image="https://static-00.iconduck.com/assets.00/results-icon-256x256-cb2c2kkw.png"/>
                         <MyCard value="12.1ha" title="New area" description="This year" image="https://static-00.iconduck.com/assets.00/results-icon-256x256-cb2c2kkw.png"/>
