78adf1c6a03209b0c857ee802bef722fbd5218be
Mircea Mihai Adrian <62558071+ayymomma@users.noreply.github.com>
2023-01-18 09:50:21 +0200

    add top info and log out to contracts page

--- src/components/Contracts.js
+++ src/components/Contracts.js
@@ -5,8 +5,10 @@ export default function Contracts() {
     return (
         <div className='contractsPage'>
             <Sidenav/>
-            <div className='contracts'>
-                <h1>Contracts</h1>
+            <div className='contractsText'>
+                    <h1>Contracts</h1>
+                    {/* add a href for log out */}
+                    <a onClick={() => {localStorage.removeItem('token'); localStorage.removeItem('username')}} href="/login">Log out</a>
             </div>
         </div>
     )
